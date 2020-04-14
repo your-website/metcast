bee51dac17fb7bcc96b0c38b46b9d882dcc3aeaa
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-04-14 22:11:35 +0700

    Updates

---
+++ precache-manifest.39bf769c1317420c1589252b74c1a279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b647e00a8dc76d07198c2ba537c231",
+    "url": "/metcast/index.html"
+  },
+  {
+    "revision": "108ff8c02ccc5b5e1aad",
+    "url": "/metcast/static/css/main.9afd29f7.chunk.css"
+  },
+  {
+    "revision": "0dac1c9037c2c6c2d15d",
+    "url": "/metcast/static/js/2.2acdaebd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/metcast/static/js/2.2acdaebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108ff8c02ccc5b5e1aad",
+    "url": "/metcast/static/js/main.e814b72d.chunk.js"
+  },
+  {
+    "revision": "e8cbedabfcda4c91309b",
+    "url": "/metcast/static/js/runtime-main.2f8c1fd8.js"
+  }
+]);
